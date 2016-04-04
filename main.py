0e15d728739fc8723cb8d0c267f47b6a6ade870c
fisk <mcfisk@ymail.com>
2016-04-03 20:42:18 -0500

    started implementing 'get all posts since a certain date' functionality

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ class Main():
     def __init__(self):
         print("Working")
         #self.url      = "http://e621.net/post/index.json?limit=5"
-        self.url      = "http://e621.net/pool/show.json?id=6520"
+        #self.url      = "http://e621.net/pool/show.json?id=6520"
+        self.url = "https://e621.net/post/index.json?limit=100page=1"
         self.values   = ["author", "file_url", "tags"]
         self.scrapper = e621.Scrapper(self.url, verbose=False)
         
-        self.scrapper.getPoolInfo()
-        self.scrapper.getPostInfo()
-        self.printPoolInfo()
+        #self.scrapper.getPoolInfo()
+        #self.scrapper.getPostInfo()
+        self.scrapper.getAllSinceYesterday()
+        #self.printPoolInfo()
 
     def printPoolInfo(self):
         print(self.scrapper.data["name"])
--- scrappers/e621.py
+++ scrappers/e621.py
@@ -24,26 +24,60 @@ class Scrapper():
             #print(self.responce)
             self.printResponce()
 
-    def getAllSinceYesterday():
-        pass
+    def getAllSinceYesterday(self):
+        #self.printIndexResponce()
+        # get todays date and subtract 1 day to get yesterdays date
+        today = datetime.datetime.now()    # this time object represents the exact date and time of when it was created!
+        delta = datetime.timedelta(days=1) # timedeltas are used to objectify amounts of time in terms of dates!
+        yestr = today - delta
+        print(self.responce[0]["created_at"])
+        time = self.responce[0]["created_at"]["s"], self.responce[0]["created_at"]["n"]
+        post = datetime.datetime.fromtimestamp(float("{s}.{n}".format(s=time[0], n=time[1])))
+        print(post)
+        print(self.responce[0]["file_url"])
 
-    def getAllSinceDate():
-        pass
+        print(yestr < post)
+        self.getAllSinceDate(date=yestr)
+
+    def getAllSinceDate(self, date):
+        posts = []
+        for post in self.responce:
+            jsonDate = post["created_at"]["s"], post["created_at"]["n"]
+            postDate = datetime.datetime.fromtimestamp(float("{s}.{n}".format(s=jsonDate[0], n=jsonDate[1])))
+            if postDate > date:
+                posts.append({})
+                
+    
+    def printIndexResponce(self):
+        print("------{num} posts------".format(num=len(self.responce)))
+        for post in self.responce:
+            print("#=#=#=#=#=#=#=#=#=#")
+            for key in post:
+                print("{key} => {value}".format(key=key, value=post[key]))
+        
+        print("------{num} posts------".format(num=len(self.responce)))
 
     def printResponce(self):
+        for key in self.responce:
+            print(key)
+
+    def printData(self):
         for key in self.data:
             print(key, " = ", self.data[key])
+
+    def addPostDataByKey(self):
+        pass
 
     def getPoolInfo(self):
         # Parse self.responce to extract information about the pool
         for key in self.dataKeys:
             self.data[key] = self.responce[key]
 
-    def getPostInfo(self):
+    def getPostInfo(self, posts=self.request["posts"]):
         # this function returns requested values from a list of supplied keys
         # example: ["author", "file_url"] would return these key values out of the responce data
         self.data["posts"] = []
-        for post in self.responce["posts"]: # for every post in the list of posts
+        for post in posts:                  # for every post in the list of posts
             self.data["posts"].append({})   # add an empty dict to the post data; this will serve as a container for the requested data
             for key in self.postKeys:       # then for every key in the list of requested keys
                 self.data["posts"][-1][key] = post[key] # Select the most recently added post data dict, and add the requested key along with its value
